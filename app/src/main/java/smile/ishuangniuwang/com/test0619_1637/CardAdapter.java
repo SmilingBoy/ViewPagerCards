ee14be570da9ac8619005b6ce4b9bdeab17396c2
Simle <18463384056@163.com>
2017-06-19 17:42:22 +0800

    第一次提交

---
+++ app/src/main/java/smile/ishuangniuwang/com/test0619_1637/CardAdapter.java
@@ -0,0 +1,15 @@
+package smile.ishuangniuwang.com.test0619_1637;
+
+
+import android.support.v7.widget.CardView;
+
+public interface CardAdapter {
+
+    int MAX_ELEVATION_FACTOR = 8;
+
+    float getBaseElevation();
+
+    CardView getCardViewAt(int position);
+
+    int getCount();
+}
---
+++ app/src/main/java/smile/ishuangniuwang/com/test0619_1637/CardItem.java
@@ -0,0 +1,21 @@
+package smile.ishuangniuwang.com.test0619_1637;
+
+
+public class CardItem {
+
+    private int mTextResource;
+    private int mTitleResource;
+
+    public CardItem(int title, int text) {
+        mTitleResource = title;
+        mTextResource = text;
+    }
+
+    public int getText() {
+        return mTextResource;
+    }
+
+    public int getTitle() {
+        return mTitleResource;
+    }
+}
